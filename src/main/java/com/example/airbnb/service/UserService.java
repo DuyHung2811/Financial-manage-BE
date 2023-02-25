632bbf8eecc8c3b4636a6ab0ac4fbe6923c29fee
DuyHung2811 <112716865+DuyHung2811@users.noreply.github.com>
2023-02-26 01:38:27 +0700

    da sua code

---
+++ src/main/java/com/example/airbnb/service/UserService.java
@@ -0,0 +1,35 @@
+package com.example.airbnb.service;
+
+import com.example.airbnb.model.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+import java.util.Optional;
+
+public interface UserService extends UserDetailsService {
+    User save(User user);
+
+    User lockUser(Long id, User user);
+
+    User unLockUser(Long id, User user);
+
+    Iterable<User> findAll();
+
+    User findByUsername(String username);
+
+    User getCurrentUser();
+
+    Optional<User> findById(Long id);
+
+    UserDetails loadUserById(Long id);
+
+    boolean checkLogin(User user);
+
+    boolean isRegister(User user);
+
+    boolean isCorrectConfirmPassword(User user);
+
+    boolean existsByUsername(String username);
+
+
+}
